bd483a735f1f53b806a449fab14265f93bbcfa7a
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-02-24 23:43:19 +0200

    init commit

---
+++ SeriesSearcher/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesSearcher
+{
+    public class SeariesSearcher
+    {
+        public const String BASE_URL = "http://services.tvrage.com/feeds/%s.php?%s=%s";
+
+        public SeariesSearcher()
+        {
+
+        }
+
+        public String[] searchShowsByQuery(String query)
+        {
+            String[] returned = new String[3];
+            returned[0] = "test1";
+            returned[1] = "test2";
+            returned[2] = "test3";
+
+            return returned;
+        }
+    }
+}

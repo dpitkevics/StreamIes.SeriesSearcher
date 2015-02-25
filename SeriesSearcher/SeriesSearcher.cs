e04221febe36fb164d612d264d80b9678412f4ba
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-02-25 15:41:14 +0200

    xml ir now successfully parsed

--- SeriesSearcher/SeriesSearcher.cs
+++ SeriesSearcher/SeriesSearcher.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using System.Xml;
 
 namespace StreamIes.SeriesSearcher
 {
@@ -12,9 +13,9 @@ namespace StreamIes.SeriesSearcher
     {
         public const String BASE_URL = "http://services.tvrage.com/feeds/{0}.php?{1}={2}";
 
-        private Func<int> callback;
+        private Func<Results, int> callback;
 
-        public Searcher(Func<int> callback)
+        public Searcher(Func<Results, int> callback)
         {
             this.callback = callback;
         }
@@ -26,9 +27,43 @@ namespace StreamIes.SeriesSearcher
             WebClient client = new WebClient();
             String xmlData = client.DownloadString(requestUrl);
 
-            Console.WriteLine(xmlData);
+            return this.callback(this.ParseShowsSearchResult(xmlData));
+        }
 
-            return this.callback();
+        public Results ParseShowsSearchResult(String xmlData)
+        {
+            Results results = new Results();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlData);
+
+            XmlNodeList showNodeList = xmlDoc.SelectNodes("/Results/show");
+            foreach (XmlNode showNode in showNodeList)
+            {
+                Show show = new Show();
+                show.showId = Convert.ToInt16(showNode.SelectSingleNode("showid").InnerText);
+                show.name = showNode.SelectSingleNode("name").InnerText;
+                show.link = showNode.SelectSingleNode("link").InnerText;
+                show.country = showNode.SelectSingleNode("country").InnerText;
+                show.started = Convert.ToInt16(showNode.SelectSingleNode("started").InnerText);
+                show.ended = Convert.ToInt16(showNode.SelectSingleNode("ended").InnerText);
+                show.seasons = Convert.ToInt16(showNode.SelectSingleNode("seasons").InnerText);
+                show.status = showNode.SelectSingleNode("status").InnerText;
+                show.classification = showNode.SelectSingleNode("classification").InnerText;
+
+                XmlNodeList genreNodeList = showNode.SelectNodes("genres/genre");
+                foreach (XmlNode genreNode in genreNodeList)
+                {
+                    Genre genre = new Genre();
+                    genre.title = genreNode.InnerText;
+
+                    show.genres.Add(genre);
+                }
+
+                results.showList.Add(show);
+            }
+
+            return results;
         }
     }
 }
